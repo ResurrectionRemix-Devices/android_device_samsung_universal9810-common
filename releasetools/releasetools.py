4c89e6a42d68ff0ee7c8ee14987f6b2cc93c3eef
Jesse Chan <jc@lineageos.org>
2018-04-20 00:11:15 -0700

    universal9810: Assert on vendor image version
    Change-Id: Ia74576e547bb3e45d13c06b6255f42d94b5e0e14

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -1,3 +1,20 @@
+def FullOTA_Assertions(info):
+    info.script.AppendExtra('ifelse(is_mounted("/vendor"), unmount("/vendor"));');
+    info.script.AppendExtra('mount("ext4", "EMMC", "/dev/block/platform/11120000.ufs/by-name/VENDOR", "/vendor");');
+    info.script.AppendExtra('ifelse(file_getprop("/vendor/build.prop","ro.vendor.build.version.sdk") != "29",');
+    info.script.AppendExtra('unmount("/vendor");');
+    info.script.AppendExtra('ui_print(" ");');
+    info.script.AppendExtra('ui_print("=============================================");');
+    info.script.AppendExtra('ui_print("=============================================");');
+    info.script.AppendExtra('ui_print("                    ERROR:                   ");');
+    info.script.AppendExtra('ui_print("           Vendor image on the device        ");');
+    info.script.AppendExtra('ui_print("               is NOT compatible             ");');
+    info.script.AppendExtra('ui_print("           Expected Android 10 vendor        ");');
+    info.script.AppendExtra('ui_print("=============================================");');
+    info.script.AppendExtra('ui_print("============NO CHANGE HAS BEEN MADE==========");');
+    info.script.AppendExtra('abort(" "););');
+    info.script.AppendExtra('unmount("/vendor");');
+
 def FullOTA_PostValidate(info):
     info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/platform/11120000.ufs/by-name/SYSTEM");');
     info.script.AppendExtra('run_program("/sbin/resize2fs", "/dev/block/platform/11120000.ufs/by-name/SYSTEM");');
