29962c5d092420ac18513985790d7f28bc2622a0
synt4x93 <synt4x.93@gmail.com>
2020-06-03 23:59:01 +0100

    universal9810: vibrator: build custom HAL
    - taken from https://github.com/whatawurst/android_device_samsung_exynos9820-common
    
    Signed-off-by: Rob Burton <synt4x.93@gmail.com>
    Change-Id: If5d1674e50f58a29bd65097b00f36829c2cd8cee

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -79,6 +79,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lineage.livedisplay@2.0-service.samsung-exynos
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.3-service.9810
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.1-service.9810
